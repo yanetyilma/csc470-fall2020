a34ebd1f7f6f6d4e6d09c0db9013cbe3e90529ee
yanetyilma <54584500+yanetyilma@users.noreply.github.com>
2020-09-18 16:02:46 -0400

    "game02"

---
+++ exercises/game02/Assets/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using Random = UnityEngine.Random;
+//using System.Random;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject heartPrefab;
+    public GameObject moonPrefab;
+    public GameObject starPrefab;
+    GameObject ground;
+
+    float makeMoonTimer = 1.5f;
+    float makeMoonRate = 1.5f;
+
+    float makeStarTimer = 1f;
+    float makeStarRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        makeMoonTimer -= Time.deltaTime;
+        if (makeMoonTimer < 0)
+        {
+            Vector3 pos = new Vector3(ground.transform.position.x + Random.Range(-10, 10)
+                                , ground.transform.position.y + 250,
+                                ground.transform.position.z + Random.Range(-10, 10));
+
+            GameObject drop = Instantiate(moonPrefab, pos, Quaternion.identity);
+
+            Destroy(drop, 7f);
+
+            //Reset timer
+            makeMoonTimer = makeMoonRate;
+        }
+        makeStarTimer -= Time.deltaTime;
+        if (makeStarTimer < 0)
+        {
+            Vector3 pos = new Vector3(ground.transform.position.x + Random.Range(-20, 20)
+                                , ground.transform.position.y + 250,
+                                ground.transform.position.z + Random.Range(-20, 20));
+
+            GameObject drop = Instantiate(starPrefab, pos, Quaternion.identity);
+
+            Destroy(drop, 7f);
+
+            //Reset timer
+            makeStarTimer = makeStarRate;
+
+        }
+
+    }
+
+    public void MakeMoreHearts()
+    {
+        Vector3 pos = new Vector3(ground.transform.position.x + Random.Range(-20, 20)
+            , ground.transform.position.y + 250,
+            ground.transform.position.z + Random.Range(-20, 20));
+        Instantiate(heartPrefab, pos, Quaternion.identity);
+    }
+
+    public void PressStart()
+    {
+        SceneManager.LoadScene("level");
+    }
+}
---
+++ exercises/game02/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 60f;
+    float rotateSpeed = 100f;
+    int score = 0; 
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0);
+
+        transform.Translate(transform.forward * Time.deltaTime * speed);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hearts"))
+        {
+            Destroy(other.gameObject);
+
+            score++;
+            scoreText.text = score.ToString();
+
+        }
+        
+    }
+
+}
