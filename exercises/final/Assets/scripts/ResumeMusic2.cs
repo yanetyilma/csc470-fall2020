454f017bb096a53dd8d1025ef7fb4455b408485e
yanetyilma <54584500+yanetyilma@users.noreply.github.com>
2020-12-03 15:55:55 -0500

    "final draft 4"

---
+++ exercises/final/Assets/scripts/ResumeMusic2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeMusic2 : MonoBehaviour
+{
+    void Awake()
+    {
+        BgMusic2.Instance.gameObject.GetComponent<AudioSource>().Play();
+    }
+}
---
+++ exercises/final/Assets/scripts/StopMusic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        BgMusic.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+}
